58d99a98684c932f54a977bbd147a20d66097943
Студент 3 < 3@DESKTOP-PS97MJ6>
2022-12-03 11:33:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp4;
+
+Work work = new();
+void GetMenu()
+{
+    Console.WriteLine();
+    Console.WriteLine($"1) Получить все числа которые встречаюстя в массиве не чётное количество раз");
+    Console.WriteLine($"2) Получить все числа которые встречаюстя в массиве чётное количество раз");
+    Console.WriteLine($"3) Создать свой массив чисел");
+    Console.WriteLine();
+}
+void UseMenu()
+{
+    while (true)
+    {
+        GetMenu();
+        string enter = Console.ReadLine();
+        bool check = int.TryParse(enter, out var result);
+        Console.WriteLine();
+        int[] ints = new int[] { 1, 1, 2, 3, 4, 5, 6, 7, 8, 4, 3 };
+        if (result == 1)
+        {
+            work.OddNumber(ints);
+            return;
+        }
+        else if (result == 2)
+        {
+            work.EvenNumber(ints);
+            return;
+        }
+        else
+        {
+            Console.WriteLine($"Выберите пункт меню");
+        }
+    }
+}
+UseMenu();
---
+++ ConsoleApp4/Work.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Work
+    {
+        public void OddNumber(int[] arrayNumbers)
+        {
+            List<int> oddNumbers = new();
+            for(int i = 0; i < arrayNumbers.Length; i++)
+            {
+                int counter = 0;
+                foreach(int x in arrayNumbers)
+                {
+                    if(arrayNumbers[i] == x)
+                    {
+                        counter++;
+                    }
+                }
+                if (counter % 2 == 0) { }
+                else
+                {
+                    oddNumbers.Add(arrayNumbers[i]);
+                }
+            }
+            for(int i = 0; i < oddNumbers.Count; i++)
+            {
+                Console.Write(oddNumbers[i]);
+                if (oddNumbers.Count - 1 == i)
+                {
+                    Console.Write(".");
+                }
+                else { Console.Write(","); }
+            }            
+        }
+        public void EvenNumber(int[] arrayNumbers)
+        {
+            List<int> evenNumber = new();
+            for (int i = 0; i < arrayNumbers.Length; i++)
+            {
+                int counter = 0;
+                foreach (int x in arrayNumbers)
+                {
+                    if (arrayNumbers[i] == x)
+                    {
+                        counter++;
+                    }
+                }
+                if (counter % 2 == 0)
+                {
+                    if (evenNumber.Contains(arrayNumbers[i]) == false)
+                    {
+                        evenNumber.Add(arrayNumbers[i]);
+                    }
+                }
+                else { };
+            }
+            for (int i = 0; i < evenNumber.Count; i++)
+            {
+                Console.Write(evenNumber[i]);
+                if (evenNumber.Count - 1 == i)
+                {
+                    Console.Write(".");
+                }
+                else { Console.Write(","); }
+            }
+        }
+        public int[] CreateUserArray()
+        {
+            Console.WriteLine($"Введите числа которые хотите записать в массив (числа нужно записать через пробел либо запятую");
+            string enter = Console.ReadLine();
+            var split= enter.Split(' ', ',');
+            
+            return ;
+        }
+    }
+}
